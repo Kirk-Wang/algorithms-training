c81d4552c307060cc6eea05f2aadcf8ffa093199
Kirk.Wang <kirk.w.wang@gmail.com>
2019-09-21 15:56:33 +0800

    feat(gray-code.ts): Gray Code

--- src/array/gray-code.ts
+++ src/array/gray-code.ts
@@ -1,5 +1,24 @@
-
-
 export function grayCode(n: number) {
-  return n
+  if (n === 0) {
+    return [0]
+  }
+  const calc = (n: number): string[] => {
+    if (n === 1) {
+      return ['0', '1']
+    } else {
+      const prev: string[] = calc(n - 1)
+      const len = Math.pow(2, n)
+      const maxPos = len - 1
+      const half = len / 2
+      const curGrayCode: string[] = []
+      // 砍一半
+      for (let i = 0; i < half; i++) {
+        // 0
+        curGrayCode[i] = `0${prev[i]}`
+        curGrayCode[maxPos - i] = `1${prev[i]}`
+      }
+      return curGrayCode
+    }
+  }
+  return calc(n).map(item => parseInt(item, 2))
 }
--- test/array/gray-code.test.ts
+++ test/array/gray-code.test.ts
@@ -1,7 +1,16 @@
 import { grayCode } from '../../src/algorithms-training'
 
 describe('grayCode test', () => {
+  it(`n:0`, () => {
+    expect(grayCode(0)).toEqual([0])
+  })
   it(`n:1`, () => {
-    expect(grayCode(1)).toBe(true)
+    expect(grayCode(1)).toEqual([0, 1])
+  })
+  it(`n:2`, () => {
+    expect(grayCode(2)).toEqual([0, 1, 3, 2])
+  })
+  it(`n:3`, () => {
+    expect(grayCode(3)).toEqual([0, 1, 3, 2, 6, 7, 5, 4])
   })
 })
